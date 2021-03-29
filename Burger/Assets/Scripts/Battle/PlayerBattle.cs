c8d6821448c9d12de4bd93ec39bc10becea4f1a1
Cat <cat.calupas@gmail.com>
2021-03-29 20:58:35 +0200

    battle system not working

--- Burger/Assets/Scripts/Battle/PlayerBattle.cs
+++ Burger/Assets/Scripts/Battle/PlayerBattle.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class PlayerBattle : MonoBehaviour
 {
     //Attack, Animation, Health update (gradual reduction)
+    [SerializeField] int vDamage;
+    [SerializeField] int dDamage;
+    [SerializeField] int tHeal;
 
-    public void ViktoriaAttack()
+    public void ViktoriaAttack(EnemyBattle enemy)
     {
         //animate playerSprite
         //reduce enemy health by attack points (gradual reduction)
+        enemy.TakeDamage(vDamage);
     }
 
-    public void DoraAttack()
+    public void DoraAttack(EnemyBattle enemy)
     {
         //animate playerSprite
         //reduce enemy health by attack points OR 30% chance of critical damage (gradual reduction)
     }
 
-    public void TamaraAttack()
+    public void TamaraAttack(EnemyBattle enemy)
     {
         //animate playerSprite
         //heal one player
