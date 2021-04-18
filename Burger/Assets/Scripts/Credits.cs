eb580eb6d73d99498f3399bc59a9c1311b3ba077
Cat <cat.calupas@gmail.com>
2021-04-18 20:09:00 +0200

    update download button with new link

--- Burger/Assets/Scripts/Credits.cs
+++ Burger/Assets/Scripts/Credits.cs
@@ -4,18 +4,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Net;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    public Slider progressBar;
+    public Text savedToText;
+
     public void Download()
     {
         WebClient webClient = new WebClient();
         webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
-        webClient.DownloadFileAsync(new Uri("https://catherinejoyc.github.io/portfolio/assets/images/123.jpg"), @"D:\123.jpg");
+        webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+        webClient.DownloadFileAsync(new Uri("https://cdn.discordapp.com/attachments/833394647932993537/833395026507202640/Dives_-_Burger_320.mp3"), Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Dives-Burger.mp3");
     }
+    private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+    {
+        progressBar.value = e.ProgressPercentage;
+    }
+
 
     private void Completed(object sender, AsyncCompletedEventArgs e)
     {
-        print("Download completed! -");
+        savedToText.text = "Download complete! Saved to: " + Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Dives-Burger.mp3";
     }
 }
